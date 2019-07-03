ec536e9fdd8f89b7ab07520ba13a23f47ec7a98b
Maczek <pawelfraczyk@MacBook-Pro-Maczek-8.local>
2019-07-03 20:46:55 +0200

    Added condtion and default props

--- src/components/price.js
+++ src/components/price.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Price = ({ cents }) => (
-  <span className="ui green ribbon label">$ {cents / 100}</span>
+  <span className="ui green ribbon label">
+    ${cents / 100} {cents < 3000 && "!"}
+  </span>
 );
 
 export default Price;
