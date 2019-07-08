614ab65d267d0a6cbed278866580e153e169f722
Maczek <pawelfraczyk@MacBook-Pro-Maczek-8.local>
2019-07-08 22:18:01 +0200

    Added fields with number type

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -3,7 +3,10 @@ import React, { Component } from "react";
 export default class GameForm extends Component {
   state = {
     name: "",
-    description: ""
+    description: "",
+    price: 0,
+    duration: 0,
+    players: ""
   };
 
   handleSubmit = e => {
@@ -11,7 +14,13 @@ export default class GameForm extends Component {
     console.log(this.state);
   };
 
-  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+  handleChange = e =>
+    this.setState({
+      [e.target.name]:
+        e.target.type === "number"
+          ? parseInt(e.target.value, 10)
+          : e.target.value
+    });
 
   render() {
     return (
@@ -37,6 +46,39 @@ export default class GameForm extends Component {
             value={this.state.description}
             onChange={this.handleChange}
           />
+        </div>
+
+        <div className="three fields">
+          <div className="field">
+            <label htmlFor="price">Price (in cents) </label>
+            <input
+              type="number"
+              id="price"
+              name="price"
+              value={this.state.price}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="duration">Duration (in minutes)</label>
+            <input
+              type="number"
+              id="duration"
+              name="duration"
+              value={this.state.duration}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className="field">
+            <label htmlFor="players">Number of players</label>
+            <input
+              type="text"
+              id="players"
+              name="players"
+              value={this.state.players}
+              onChange={this.handleChange}
+            />
+          </div>
         </div>
 
         <button className="ui button" type="submit">
