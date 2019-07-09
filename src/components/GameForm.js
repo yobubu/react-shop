fa9918e297d10f4ed9ed1e7f37d55b0f5c0bc3af
Pawel Fraczyk <605935@edc.dk>
2019-07-09 08:44:02 +0200

    Added radio buttons

--- src/components/GameForm.js
+++ src/components/GameForm.js
@@ -6,6 +6,12 @@ const tags = [
   { _id: 3, name: "family" }
 ]
 
+const genres = [
+  { _id: 1, name: "abstract" },
+  { _id: 2, name: "euro" },
+  { _id: 3, name: "ameritrash" }
+]
+
 export default class GameForm extends Component {
   state = {
     name: "",
@@ -14,7 +20,8 @@ export default class GameForm extends Component {
     duration: 0,
     players: "",
     featured: false,
-    tags: []
+    tags: [],
+    genres: 1
   };
 
   handleSubmit = e => {
@@ -46,6 +53,8 @@ export default class GameForm extends Component {
     this.state.tags.includes(tag._id)
       ? this.setState({ tags: this.state.tags.filter(id => id !== tag._id) })
       : this.setState({ tags: [...this.state.tags, tag._id] })
+
+  handleGenreChange = genre => this.setState({ genre: genre._id })
 
   render() {
     return (
@@ -129,6 +138,22 @@ export default class GameForm extends Component {
               </div>
             )
             )
+          }
+        </div>
+
+        <div className="field">
+          <label>Genres</label>
+          {
+            genres.map(genre => (
+              <div key={genre._id} className="inline field">
+                <input
+                  id={`genre-${genre._id}`}
+                  type="radio"
+                  checked={this.state.genre === genre._id}
+                  onChange={() => this.handleGenreChange(genre)} />
+                <label htmlFor={`genre-${genre._id}`}>{genre.name}</label>
+              </div>
+            ))
           }
         </div>
 
