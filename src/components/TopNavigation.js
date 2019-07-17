5566282bd7b12261c68461259d30baf4d09ee42c
Maczek <pawelfraczyk@MacBook-Pro-Maczek-8.local>
2019-07-17 17:33:28 +0200

    Navigate between routes with link component

--- src/components/TopNavigation.js
+++ src/components/TopNavigation.js
@@ -1,21 +1,20 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
 
-const TopNavigation = ({ showGameForm }) => (
+const TopNavigation = () => (
   <div className="ui secondary pointing menu">
-    <a href="/" className="item">
+    <NavLink exact to="/" className="item">
       BGShop
-    </a>
-    <a className="item" onClick={showGameForm}>
+    </NavLink>
+    <NavLink exact to="/games" className="item">
+      Games
+    </NavLink>
+    <NavLink exact to="/games/new" className="item">
       <i className="icon plus" />
       Add New Game
-    </a>
+    </NavLink>
   </div>
 );
-
-TopNavigation.propTypes = {
-  showGameForm: PropTypes.func.isRequired
-};
 
 export default TopNavigation;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./components/GamesPage";
+import App from "./components/App";
 
 import "semantic-ui-css/semantic.min.css";
 
