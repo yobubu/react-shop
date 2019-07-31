08262f54ce185ad07058d88e42468d5cc8535b1b
Maczek <pawelfraczyk@MacBook-Pro-Maczek-8.local>
2019-07-31 22:55:18 +0200

    added to cart sent to mongodb

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -5,54 +5,15 @@ import userOnly from "../middlewares/userOnly";
 
 const router = express.Router();
 
-// router.post("/", authenticate, userOnly, (req, res) => {
-//   const db = req.app.get("db");
-
-//   db.collection("users").insertOne(req.body.game, (err, r) => {
-//     if (err) {
-//       res.status(500).json({ errors: { global: err } });
-//       return;
-//     }
-
-//     res.json({ game: r.ops[0] });
-//   });
-// });
-
-router.put("/", authenticate, userOnly, (req, res) => {
-  const db = req.app.get("db");
-  const { _id, email, password, role, ...game } = req.body.game;
-
-  if (Object.keys(errors).length === 0) {
-    db.collection("users").findOneAndUpdate(
-      { _id: new mongodb.ObjectId(req.params._id) },
-      { $set: game },
-      { returnOriginal: false },
-      (err, r) => {
-        if (err) {
-          res.status(500).json({ errors: { global: err } });
-          return;
-        }
-
-        // res.json({ game: r.value });
-      }
-    );
-  } else {
-    res.status(400).json({ errors });
-  }
-});
-
-router.delete("/:_id", authenticate, userOnly, (req, res) => {
+router.put("/", authenticate, userOnly, req => {
+  const _id = req.body.user._id;
+  const gameId = req.body.game._id;
   const db = req.app.get("db");
 
-  db.collection("games").deleteOne(
-    { _id: new mongodb.ObjectId(req.params._id) },
-    err => {
-      if (err) {
-        res.status(500).json({ errors: { global: err } });
-        return;
-      }
-
-      res.json({});
+  db.collection("users").findOneAndUpdate(
+    { _id: new mongodb.ObjectId(_id) },
+    {
+      $push: { cart: gameId }
     }
   );
 });
