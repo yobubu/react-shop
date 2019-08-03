af546924131c524b5d72d39a16b9fae9b7fd0565
Maczek <pawelfraczyk@MacBook-Pro-Maczek-8.local>
2019-08-03 13:30:25 +0200

    Updating Modal text

--- src/components/ModalPage.js
+++ src/components/ModalPage.js
@@ -19,8 +19,14 @@ const ModalPage = ({ open, onClose }) => (
         <Image wrapped size="medium" src={ModalPhoto} />
         <Modal.Description>
           <Header>Thank you for coming to my app</Header>
-          <p>Please read first about the features implemented.</p>
-          <p>Later try to find some good vibes for yourself :)</p>
+          <p>
+            Please watch first video about the admin priviliges of adding,
+            updating and removing games.
+          </p>
+          <p>
+            Later try to sign up and login as regular user. At this moment I'm
+            working on shopping cart that will be avaiable for users.
+          </p>
           <p>
             I will answer all your question about my skills with pleasure so
             don't forget to contact me after that :)
