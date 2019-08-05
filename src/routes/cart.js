ba7519b7d57712db0a92d164780b56add03f3fb9
Maczek <pawelfraczyk@MacBook-Pro-Maczek-8.local>
2019-08-05 21:50:42 +0200

    removing game from cart

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -41,4 +41,21 @@ router.put("/", authenticate, userOnly, (req, res) => {
   );
 });
 
+router.put("/:_id", authenticate, userOnly, (req, res) => {
+  const _id = req.params._id;
+  const _gameId = req.body.game;
+  const db = req.app.get("db");
+  db.collection("users").updateMany(
+    { _id: new mongodb.ObjectId(_id) },
+    { $pullAll: { cart: [_gameId] } },
+    (err, cart) => {
+      if (err) {
+        res.status(500).json({ errors: { global: err } });
+        return;
+      }
+      res.json({ cart });
+    }
+  );
+});
+
 export default router;
