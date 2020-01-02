5ef08e17a37594725071c090624cfeb6d3df772c
Pawel Fraczyk <pawel@fraczyk.eu>
2020-01-02 22:51:18 +0100

    changing names

--- terraform/aws-codedeploy.tf
+++ terraform/aws-codedeploy.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "codedeploy-service-role" {
+resource "aws_iam_role" "codedeploy_service_role" {
   name = "codedeploy-service-role"
 
   assume_role_policy = <<EOF
@@ -18,20 +18,19 @@ resource "aws_iam_role" "codedeploy-service-role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "AWSCodeDeployRole" {
+resource "aws_iam_role_policy_attachment" "bgshop_AWSCodeDeployRole" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
-  role       = aws_iam_role.codedeploy-service-role.name
+  role       = aws_iam_role.codedeploy_service_role.name
 }
 
-resource "aws_codedeploy_app" "bgshop-app" {
+resource "aws_codedeploy_app" "bgshop_app" {
   name             = "bgshop-app"
 }
 
-resource "aws_codedeploy_deployment_group" "bgshop-deploy-group" {
-  app_name              = aws_codedeploy_app.bgshop-app.name
+resource "aws_codedeploy_deployment_group" "bgshop_deploy_group" {
+  app_name              = aws_codedeploy_app.bgshop_app.name
   deployment_group_name = "bgshop-deploy-group"
-
-  service_role_arn      = aws_iam_role.codedeploy-service-role.arn
+  service_role_arn      = aws_iam_role.codedeploy_service_role.arn
 
   ec2_tag_set {
     ec2_tag_filter {
