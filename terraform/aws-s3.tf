2834d6478e0633421ea998b86f2c0c6c3107d6b6
Pawel Fraczyk <pawel@fraczyk.eu>
2020-01-02 20:44:35 +0100

    creating bundle-bucket

---
+++ terraform/aws-s3.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "bgshop_codedeploy_bucket" {
+  bucket = "bgshop-codedeploy-bucket"
+  acl    = "private"
+
+  region = "eu-central-1"
+
+  versioning {
+    enabled = false
+  }
+
+  lifecycle {
+    prevent_destroy = false
+  }
+
+  tags {
+    Name      = "S3 Bgshop CodeDeploy Bucket"
+    Terraform = true
+    Calluna   = true
+  }
+}
