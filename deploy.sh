86c4711e45a599165fcf1408cc87ce49b1d69460
pawelfraczyk87 <pawelfraczyk87@gmail.com>
2021-02-21 15:21:11 +0700

    updated-ci-job-id-variable

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-aws deploy push --application-name $APPLICATION_NAME --s3-location s3://$AWS_S3_BUCKET/$APPLICATION_NAME/$CI_BUILD_ID.zip
-ID=$(aws deploy create-deployment --application-name $APPLICATION_NAME --deployment-group-name $DEPLOYMENT_GROUP_NAME --s3-location bucket=$AWS_S3_BUCKET,key=$APPLICATION_NAME/$CI_BUILD_ID.zip,bundleType=zip --output text --query '[deploymentId]')
+aws deploy push --application-name $APPLICATION_NAME --s3-location s3://$AWS_S3_BUCKET/$APPLICATION_NAME/$CI_JOB_ID.zip
+ID=$(aws deploy create-deployment --application-name $APPLICATION_NAME --deployment-group-name $DEPLOYMENT_GROUP_NAME --s3-location bucket=$AWS_S3_BUCKET,key=$APPLICATION_NAME/$CI_JOB_ID.zip,bundleType=zip --output text --query '[deploymentId]')
 STATUS=$(aws deploy get-deployment --deployment-id $ID --output text --query '[deploymentInfo.status]') 
 
 while [[ $STATUS == "Created" || $STATUS == "InProgress" || $STATUS == "Pending" || $STATUS == "Queued" || $STATUS == "Ready" ]]; do
