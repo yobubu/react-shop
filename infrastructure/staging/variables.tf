a374b379c54503fd83c7da86559596d8ba998a0b
Pawel Fraczyk <pawelfraczyk87@gmail.com>
2021-04-12 21:29:35 +0700

    added-documentdb-ecr-autoscaling

--- infrastructure/staging/variables.tf
+++ infrastructure/staging/variables.tf
@@ -1,8 +1,29 @@
 variable "region" {
   type    = string
   default = "ap-southeast-1"
+  description = "AWS Region"
 }
 
 variable "project" {
+  type = string
   default = "react-shop"
+  description = "Project name eg. react-shop"
+}
+
+variable "environment" {
+  type = string
+  default = "dev"
+  description = "Environment eg. dev, qa, uat, prod"
+}
+
+variable "docdb_master_user" {
+  type = string
+  default = "sammysammy"
+  description = "User name for AWS Document DB"
+}
+
+variable "docdb_master_pass" {
+  type = string
+  default = "dummydummy"
+  description = "Password for AWS Document DB"
 }
