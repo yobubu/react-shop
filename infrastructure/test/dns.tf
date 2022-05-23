9c7da88ae6c7d5842fb03e85ac844eb0d56ed1e9
Irek Wasilewski <yobubu@wp.pl>
2022-05-23 21:53:17 +0700

    add lb acl

--- infrastructure/test/dns.tf
+++ infrastructure/test/dns.tf
@@ -10,3 +10,16 @@ resource "aws_route53_record" "Frontend" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "backend" {
+
+  allow_overwrite = true
+  name            = "backend.${var.domain}"
+  type            = "A"
+  zone_id         = data.aws_route53_zone.domain.zone_id
+  alias {
+    name                   = aws_lb.public_lb.dns_name
+    zone_id                = aws_lb.public_lb.zone_id
+    evaluate_target_health = true
+  }
+}
--- infrastructure/test/lb.tf
+++ infrastructure/test/lb.tf
@@ -14,6 +14,24 @@ resource "aws_lb_listener" "public_lb_http" {
   protocol          = "HTTP"
 
   default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "public_lb_https" {
+  load_balancer_arn = aws_lb.public_lb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = aws_acm_certificate.backend.arn
+
+  default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.ecs_backend.arn
   }
